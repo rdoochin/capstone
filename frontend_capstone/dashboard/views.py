c955a57d61cf8fa1d38481b06d3f9055deaab63a
rdoochin <rdoochin@tulane.edu>
2020-11-13 01:02:14 -0600

    updated index page

--- frontend_capstone/dashboard/views.py
+++ frontend_capstone/dashboard/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import context
+from django.http import Http404
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the dashboard")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'dashboard/index.html', context)
 
 
 def detail(request, question_id):
@@ -17,3 +22,11 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'dashboard/detail.html', {'question': question})
