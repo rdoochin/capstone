d1373f73996a4b0847fa1c674008cf669656b573
rdoochin <rdoochin@tulane.edu>
2020-11-13 00:24:50 -0600

    incase i mess this up

--- frontend_capstone/dashboard/models.py
+++ frontend_capstone/dashboard/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+## From tutorial     ##      ##      ##      ##
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    # This makes editing the sql in the terminal more readable.
+    def __str__(self):
+        return self.question_text
+
+    # date-time stuff.
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    # This makes editing the sql in the terminal more readable.
+    def __str__(self):
+        return self.question_text
+##      ##      ##      ##      ##       ##      ##
