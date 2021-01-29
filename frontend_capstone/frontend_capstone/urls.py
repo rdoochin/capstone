357e9fa1894bc6dbc9f85810a27c9a1e82ac0dce
rdoochin <rdoochin@tulane.edu>
2021-01-28 18:06:48 -0600

    added req.txt file

--- frontend_capstone/frontend_capstone/urls.py
+++ frontend_capstone/frontend_capstone/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
+    path('', include('dashboard.urls')),    #makes homepage route to dashboard
 ]
