2c79eca90d1f31018895d5b17024c8cfba40b5a5
rdoochin <rdoochin@tulane.edu>
2021-05-02 21:01:33 -0500

    clean up and comments

--- frontend_capstone/dashboard/urls.py
+++ frontend_capstone/dashboard/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     # ex: /dashboard/
     path('', views.index, name='index'),
-    # ex: /dashboard/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /dashboard/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /dashboard/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
